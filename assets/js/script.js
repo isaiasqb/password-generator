0734836ec3d363f08e9af20720921b3d032eaabe
IsaiasQB <isaias.qb@hotmail.com>
2022-03-11 11:43:34 -0500

    added prompts for criteria and validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,8 +8,56 @@ var randomness = function(multiplier) {
 
 
 function generatePassword (){
+  // use parseInt to convert the value of the promt to a numeric value
+  var promptLength = parseInt(prompt("How Many Characters would you like your password to have? \nPlease choose a number between 8 - 128"), 10);
+  // confirming that the value has been turned into a prompt
+  // console.log(typeof promptLength)
+  if (promptLength >= 8 && promptLength <= 128){
+    
+      var confirmSpecial = confirm("Would You like to include Special Characters?");
+      if (confirmSpecial){
+        console.log("You chose to Use Special Characters");
+      }else {
+        console.log("You chose NOT to Use Special Characters");
+      };
 
-  return randomness(letters.length);
+      var confirmUpper = confirm("Would you like to use UpperCase Characters?")
+      if(confirmUpper){
+        console.log("You chose to Use Uppercase Characters");
+      } else {
+        console.log("You chose NOT to Use Uppercase Characters");
+      };
+
+      var confirmLower = confirm("Would you like to use LowerCase Characters?")
+      if(confirmLower){
+        console.log("You chose to Use Lowercase Characters");
+      } else {
+        console.log("You chose NOT to Use Lowercase Characters");
+      };
+
+      var confirmNum = confirm("Would you like to use Numerical Characters?")
+      if(confirmNum){
+        console.log("You chose to Use Numerical Characters");
+      } else {
+        console.log("You chose NOT to Use Numerical Characters");
+      };
+
+      if(!confirmLower && !confirmNum && !confirmSpecial && !confirmUpper){
+        alert ("Please select at least ONE criteria")
+        return false;
+      }
+    return "you selected the length of: " + promptLength + " Characters";
+
+
+  } 
+  else {
+    console.log(typeof promptLength)
+    alert("Please choose a valid number");
+    return false;
+  };
+
+
+
 
 }
 
