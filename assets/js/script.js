3f3f6575347667bff57a446005b6fa2e558cf6d6
IsaiasQB <isaias.qb@hotmail.com>
2022-03-11 17:53:20 -0500

    modified the global variable declarations and for loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,52 @@
 // Assignment code here
-var specialChar = "!#$%&()*+,-./:;<=>?@[^_{}"
-var lowerChar = "abcdefghyjklmnopqrst";
-var upperChar = "ABCDEFGHYJKLMNOPQRSTWXYZ";
-var numericalChar = "1234567890";
+var specialChar = ["!#$%&()*-./:;<=>?@[_{}"]
+var upperChar = ["ABCDEFGHYJKLMNOPQRSTWXYZ"];
+var lowerChar = ["abcdefghyjklmnopqrstuvwxyz"]
+var numericalChar = ["1234567890"];
+var allCharacters = '';
+var valueGenerated = "";
 
-var randomness = function(multiplier) {
-  var randomNumber = Math.floor((Math.random() * multiplier))
-  return randomNumber;
-}
 
-var randomSpecial = specialChar[randomness(specialChar.length)];
-var randomLower = lowerChar[randomness(lowerChar.length)];
-var randomUpper = upperChar[randomness(upperChar.length)];
-var randomNumerical = numericalChar[randomness(numericalChar.length)];
-
+// var randomSpecial = specialChar[randomness(specialChar.length)];
+// var randomLower = lowerChar[randomness(lowerChar.length)];
+// var randomUpper = upperChar[randomness(upperChar.length)];
+// var randomNumerical = numericalChar[randomness(numericalChar.length)];
 
 function generatePassword (){
   // use parseInt to convert the value of the promt to a numeric value
-  var confirmedLength = parseInt(prompt("How Many Characters would you like your password to have? \nPlease choose a number between 8 - 128"), 10);
-  // confirming that the value has been turned into a prompt
-  // console.log(typeof confirmedLength)
-  if (confirmedLength >= 8 && confirmedLength <= 128){
-    console.log("you selected the length of: " + confirmedLength + " Characters");
+  var confirmLength = parseInt(prompt("How Many Characters would you like your password to have? \nPlease choose a number between 8 - 128"), 10);
+
+  if (confirmLength >= 8 && confirmLength <= 128){
+    console.log("you selected the " + typeof confirmLength +": "+ confirmLength + " Characters");
     
       var confirmSpecial = confirm("Would You like to include Special Characters?");
       if (confirmSpecial){
         console.log("You chose to Use Special Characters");
+        allCharacters = allCharacters.concat(specialChar);
       }else {
         console.log("You chose NOT to Use Special Characters");
       };
 
-      var confirmUpper = confirm("Would you like to use UpperCase Characters?")
+      var confirmUpper = confirm("Would you like to use UpperCase Characters?");
       if(confirmUpper){
         console.log("You chose to Use Uppercase Characters");
+        allCharacters = allCharacters.concat(upperChar);
       } else {
         console.log("You chose NOT to Use Uppercase Characters");
       };
 
-      var confirmLower = confirm("Would you like to use LowerCase Characters?")
+      var confirmLower = confirm("Would you like to use LowerCase Characters?");
       if(confirmLower){
         console.log("You chose to Use Lowercase Characters");
+        allCharacters = allCharacters.concat(lowerChar);
       } else {
         console.log("You chose NOT to Use Lowercase Characters");
       };
 
-      var confirmNum = confirm("Would you like to use Numerical Characters?")
+      var confirmNum = confirm("Would you like to use Numerical Characters?");
       if(confirmNum){
         console.log("You chose to Use Numerical Characters");
+        allCharacters = allCharacters.concat(numericalChar);
       } else {
         console.log("You chose NOT to Use Numerical Characters");
       };
@@ -55,22 +55,18 @@ function generatePassword (){
         alert ("Please select at least ONE criteria")
         return false;
       }
+
     
-    // var randomPass = function() {
-    // for (var i = 0; i < confirmedLength; i++) {
-    //     var value = ;
-    //     return value;
-    //   };
-    // };
-   
-    for (var i = 0; i < confirmedLength; i++){
-      var generatedPass = randomSpecial + randomLower + randomUpper + randomNumerical;
-      return generatedPass;
-    };
-    
+    for (var i = 0; i < confirmLength; i++){
+      var randomNum = Math.floor((Math.random() * allCharacters.length));
+      valueGenerated = valueGenerated + allCharacters[randomNum]
+      // var value = allCharacters + " " + randomNum;
+      // return valuePass + " - "+confirmLength; 
+      console.log(valueGenerated);
+    }   
+    return valueGenerated;
   
   } else {
-    console.log(typeof confirmedLength)
     alert("Please choose a valid number");
     return false;
   };
