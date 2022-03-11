48dfd3b294bfbc0137d2bb56d657e7cad939beb4
IsaiasQB <isaias.qb@hotmail.com>
2022-03-11 10:06:10 -0500

    test de functionality of the generate button with a random number generator

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,17 @@
 // Assignment code here
+var specialChar = "!#$%&()*+,-./:;<=>?@[]^_{|}~";
+var letters = "abcdefghyjklmnopqrst";
+var randomness = function(multiplier) {
+  var randomNumber = Math.floor((Math.random() * multiplier))
+  return randomNumber;
+}
 
+
+function generatePassword (){
+
+  return randomness(letters.length);
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,7 +22,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
