850061367c71c16fd6fcd9fcc003242fd581f275
IsaiasQB <isaias.qb@hotmail.com>
2022-03-12 14:15:33 -0500

    refactore the code with comments included

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,85 +1,94 @@
-// Assignment code here
+// Assignment code for global variables
 var specialChar = ["!#$%&()*-./:;<=>?@[_{}"]
 var upperChar = ["ABCDEFGHYJKLMNOPQRSTWXYZ"];
 var lowerChar = ["abcdefghyjklmnopqrstuvwxyz"]
 var numericalChar = ["1234567890"];
 
+// select the ul element form the HTML document with the ID of criteria list
+var criteriaList = document.querySelector("#criteria-list");
+
+// function expresson for creating a message with the user's selected criteria
+var listCriteria = function (message){
+  var criteriaInfo = document.createElement("li")
+  criteriaInfo.textContent = message;
+  criteriaList.appendChild(criteriaInfo)
+};
+
+// declare function to generate password
 function generatePassword (){
+  // reset the values of the password pool and the content of the text area for the password
+  var allCharacters = "";  
+  var valueGenerated = "";
+  criteriaList.innerHTML = null;
+
+  // Ask user for prefered criteria
   // use parseInt to convert the value of the promt to a numeric value
   var confirmLength = parseInt(prompt("How Many Characters would you like your password to have? \nPlease choose a number between 8 - 128"), 10);
-
   if (confirmLength >= 8 && confirmLength <= 128){
     console.log("you selected the " + typeof confirmLength +": "+ confirmLength + " Characters");
-    
-    var allCharacters = "";  
-    var valueGenerated = "";
-      console.log(allCharacters + "allCharacters string initial value is " + allCharacters.length);
-
 
       var confirmSpecial = confirm("Would You like to include Special Characters?");
       if (confirmSpecial){
-        console.log("You chose to Use Special Characters");
+        listCriteria("You chose to Use Special Characters");
         allCharacters = allCharacters.concat(specialChar);
       }else {
-        console.log("You chose NOT to Use Special Characters");
+        listCriteria("You chose NOT to Use Special Characters");
       };
 
       var confirmUpper = confirm("Would you like to use UpperCase Characters?");
       if(confirmUpper){
-        console.log("You chose to Use Uppercase Characters");
+        listCriteria("You chose to Use Uppercase Characters");
         allCharacters = allCharacters.concat(upperChar);
       } else {
-        console.log("You chose NOT to Use Uppercase Characters");
+        listCriteria("You chose NOT to Use Uppercase Characters");
       };
 
       var confirmLower = confirm("Would you like to use LowerCase Characters?");
       if(confirmLower){
-        console.log("You chose to Use Lowercase Characters");
+        listCriteria("You chose to Use Lowercase Characters");
         allCharacters = allCharacters.concat(lowerChar);
       } else {
-        console.log("You chose NOT to Use Lowercase Characters");
+        listCriteria("You chose NOT to Use Lowercase Characters");
       };
 
       var confirmNum = confirm("Would you like to use Numerical Characters?");
       if(confirmNum){
-        console.log("You chose to Use Numerical Characters");
+        listCriteria("You chose to Use Numerical Characters");
         allCharacters = allCharacters.concat(numericalChar);
       } else {
-        console.log("You chose NOT to Use Numerical Characters");
+        listCriteria("You chose NOT to Use Numerical Characters");
       };
 
       if(!confirmLower && !confirmNum && !confirmSpecial && !confirmUpper){
         alert ("Please select at least ONE criteria")
         return false;
-      }
+      };
 
-    
+    // for loop that creates a random selection of characters "the password" following the criteria given by the user
     for (var i = 0; i < confirmLength; i++){
       var randomNum = Math.floor((Math.random() * allCharacters.length));
       valueGenerated = valueGenerated + allCharacters[randomNum]
-      console.log(valueGenerated);
-      console.log(allCharacters);
-    }   
-    var allCharacters = '';
+      // we cna confirm the values with console.log(valueGenerated);
+      // we cna confirm the values with console.log(allCharacters);
+    };   
     return valueGenerated;
 
-  
+  // stops the application if the user did not select a valid length option
   } else {
     alert("Please choose a valid number");
     return false;
   };
-}
+};
 
-// Get references to the #generate element
+// Get references to the #generate button element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(); //the generatePassword function returns the value generated in the for loop
+  // select the testarea element form the document and insert the value of the generated password
   var passwordText = document.querySelector("#password");
- //**************reset the form to it's original value
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
